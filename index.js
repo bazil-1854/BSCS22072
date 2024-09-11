f47277de1740b39b451f8b445b41386d5d221826
bazil-1854 <bscs22072@itu.edu.pk>
2024-09-12 00:33:01 +0500

    Modified the Navbar !!

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
+// active hamburger menu 
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist")
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
 
-/*==============================================================================================*/
-/*                          Menu Bar Pop up At mobile                                           */
-/*==============================================================================================*/
-
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-    /* remove toogle icons navbar*/
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
+// remove navlist
+navlist.addEventListener("click",()=>{
+    navlist.classList.remove("active");
+    menuIcon.classList.remove("active");
+    document.body.classList.remove("open");
+})
 
 
 // Translation data
