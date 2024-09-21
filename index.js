2a7aa44fb71d80c06487bac5ad61be2d767954eb
bazil-1854 <bscs22072@itu.edu.pk>
2024-09-21 21:17:13 +0500

    Populated Coursework Section with CourseWork Javascripts Object !!

--- index.js
+++ index.js
@@ -34,3 +34,76 @@ buttons.forEach((button, index) => {
         button.classList.add('active');
     });
 });
+
+// courswork 
+const CourseWork = [
+    {
+        title: "Object-Oriented Programming",
+        description: "Learned OOP concepts such as inheritance, polymorphism, and encapsulation.",
+        iconClass: "bx bx-objects-vertical-center"
+    },
+    {
+        title: "Data Structures & Algorithms",
+        description: "Focused on sorting, searching algorithms and their complexity.",
+        iconClass: "bx bxs-building"
+    },
+    {
+        title: "Data Analysis and Algorithms",
+        description: "Gained proficiency in advanced data analysis techniques, data visualization, and pattern recognition.",
+        iconClass: "bx bxs-network-chart"
+    },
+    {
+        title: "Database Management Systems",
+        description: "Focused on SQL, NoSQL, and efficient database designs.",
+        iconClass: "bx bxs-data"
+    },
+    {
+        title: "Computer Organization and Architecture",
+        description: "Studied fundamental principles of computer organization such as CPU design, memory hierarchy, and instruction sets.",
+        iconClass: "bx bx-barcode-reader"
+    },
+    {
+        title: "Linear Algebra",
+        description: "Explored key concepts including vector spaces, matrices, and linear transformations. Focused on solving systems of linear equations.",
+        iconClass: "bx bx-math"
+    }
+];
+
+function populateCourses() {
+    const courseworkGrid = document.querySelector('.coursework-grid');
+    courseworkGrid.innerHTML = '';
+
+    CourseWork.forEach(course => {
+
+        const courseItem = document.createElement('div');
+        courseItem.className = 'course-item';
+
+        const icon = document.createElement('i');
+        icon.className = `course-icon ${course.iconClass}`;
+        courseItem.appendChild(icon);
+
+        const courseInfo = document.createElement('div');
+        courseInfo.className = 'course-info';
+
+        const courseTitle = document.createElement('h2');
+        courseTitle.textContent = course.title;
+
+        const courseDescription = document.createElement('p');
+        courseDescription.textContent = course.description;
+
+        courseInfo.appendChild(courseTitle);
+        courseInfo.appendChild(courseDescription);
+        courseItem.appendChild(courseInfo);
+        courseworkGrid.appendChild(courseItem);
+    });
+}
+ 
+
+window.onload = function () {
+    populateCourses(); 
+};
+
+const translate_page = () => { 
+    populateCourses(); 
+}
+
